80b39789d7b36aea9c8c6c6fe1e8e7d15d350b7f
ViniciusBatista1 <vinni.batista@hotmail.com>
2021-07-08 20:47:09 -0300

    ajustes no jogo da forca

--- pythonProject/venv/forca.py
+++ pythonProject/venv/forca.py
@@ -6,7 +6,25 @@ def jogar():
     print("Bem vindo no jogo da Forca!")
     print("*************")
 
-    print("Fim do jogo")
+    palavra_secreta = "banana"
+    letras_acertas = ["_", "_", "_", "_", "_", "_"]
+
+    enforcou = False
+    acertou = False
+    while(not enforcou and not acertou):
+
+        chute = input("Digite uma letra: ")
+        chute = chute.strip()
+
+        posicao = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertas[posicao] = letra
+            posicao = posicao + 1
+
+        print(letras_acertas)
+
+    print("Fim do jogo!")
 
 if(__name__ == "__main__"):
     jogar()
